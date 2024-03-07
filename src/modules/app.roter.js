ab29025fddf003e6a8f3e8fd8d1c93443d8c2fd4
ameenarabaya <ameenawaleed7@gmail.com>
2024-03-07 16:08:49 +0200

    fininshed

--- src/modules/app.roter.js
+++ src/modules/app.roter.js
@@ -1,9 +1,11 @@
 import connection from "../../DB/connection.js";
 import postRouter from './post/post.router.js';
 import userRouter from './user/user.router.js';
+import cors from 'cors';
 const initApp = (app,express)=>{
     connection()
 app.use(express.json());
+app.use(cors())
 app.use('/users',userRouter);
 app.use('/posts',postRouter);
 app.use('*',(req,res)=>{
